e6fece904d994281d08aadecb5ab1eb1148268b7
Yura Sarvas <grey-matter88@mail.ru>
2024-04-30 13:30:27 +0300

    add docstring and func title_letter

--- str_func.py
+++ str_func.py
@@ -1,3 +1,17 @@
 def upper_letter(a: str) -> str:
+    """
+    Принимает на вход строку и возвращает ее со всеми заглавными буквами
+    :param a:str
+    :return:str
+    """
     return a.upper()
+
+
+def title_letter(word: str) -> str:
+    """
+    Делает заглавными первые буквы каждого слова в строке
+    :param word:str
+    :return:str
+    """
+    return word.title()
 
